fae568ba6fad30b08218252a8e86e9bc8a28bd8a
Ansteikoku <tianzhongxiaoji75@gmail.com>
2025-12-31 08:06:26 +0900

    Update Superbase.js

--- js/Superbase.js
+++ js/Superbase.js
@@ -4,12 +4,12 @@ const SUPABASE_URL = 'https://xxxx.supabase.co'
 const SUPABASE_KEY = 'public-anon-key'
 
 export const projects = [
-  createClient(SUPABASE_URL, SUPABASE_KEY),
-  createClient(SUPABASE_URL, SUPABASE_KEY),
-  createClient(SUPABASE_URL, SUPABASE_KEY),
-  createClient(SUPABASE_URL, SUPABASE_KEY),
-  createClient(SUPABASE_URL, SUPABASE_KEY),
-  createClient(SUPABASE_URL, SUPABASE_KEY),
+  createClient(https://gewpurkaeeixoxpkvkpf.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdld3B1cmthZWVpeG94cGt2a3BmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMTY4MzEsImV4cCI6MjA4MjY5MjgzMX0.gyx7wYOj8svan1l9xebzI8h9bEFfJpf3FGOLdrnevys),
+  createClient(https://wffpnyghglmzeqqqtnzi.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmZnBueWdoZ2xtemVxcXF0bnppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMTcyMDMsImV4cCI6MjA4MjY5MzIwM30.DOzF_qOHPOzSK4tYZ8Kpf-5qzjCenognwiVqXvFkGho),
+  createClient(https://karzkluhbqgtkwjyadiz.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthcnprbHVoYnFndGt3anlhZGl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMTczOTksImV4cCI6MjA4MjY5MzM5OX0.LTi-tE-oLVgQQLFNQRRFMlbjiIUz9X0TFqCDm53Oa18),
+  createClient(https://slmzhqsplmsjzdlevqyb.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsbXpocXNwbG1zanpkbGV2cXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMTc0MDUsImV4cCI6MjA4MjY5MzQwNX0.90YRE5r46YDoKuef9g8sMPGXDDRGigxX4FVCYrWuEEk),
+  createClient(https://plawlwsdcdkekszfxeqt.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsYXdsd3NkY2RrZWtzemZ4ZXF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMTc1NTgsImV4cCI6MjA4MjY5MzU1OH0.wgwD_yI5nJ0DkLrbkd2DsCZ8o9wUKY3plHOs7BzEmB0),
+  createClient(https://dzvwgikkftfjtwkwvuij.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6dndnaWtrZnRmanR3a3d2dWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMTgwMDEsImV4cCI6MjA4MjY5NDAwMX0.KKku_wsMFZiWy8nthPmt3L9B9LN6baLVB5rZJjyWymI),
 ]
 
 export function boardToProject(boardId){
